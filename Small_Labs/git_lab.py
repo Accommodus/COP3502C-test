c92026e055887f08e25c9fc0d241a6c3c80f79c2
Cannon <cannonteaguewhitney8@gmail.com>
2023-10-31 14:53:24 -0400

    dfg dfgdfg 	Small_Labs/sci_calculator.py

---
+++ Small_Labs/git_lab.py
@@ -0,0 +1,42 @@
+""""""
+
+
+def encoder(password):
+    password = str(password)
+
+    new_password = ''
+
+    for i in password:
+        i = int(i)
+        i = i + 3
+        new_password += str(i)
+
+    return new_password
+
+
+def decoder(password):
+    pass
+
+
+def main():
+    menu = """Menu
+-------------
+1. Encode
+2. Decode
+3. Quit"""
+
+    while True:
+        print(menu)
+        menu_option = input('Please enter an option: ')
+
+        if menu_option == '1':
+            password = input('Please enter your password to encode: ')
+            print('Your password has been encoded and stored!')
+
+        elif menu_option == '2':
+            pass
+
+        elif menu_option == '3':
+            quit()
+
+
